096852a348df83b8009fc7a3f8d9b3724d56049c
Talha Tariq <tariqtalha936@gmail.com>
2024-09-16 23:48:15 +0500

    Rotate Array

--- main.py
+++ main.py
@@ -76,3 +76,16 @@ while True:
         
     else:
         print("Invalid Choice!")
+
+#Rotate Array
+class Solution(object):
+    def rotate(self, nums, k):
+        n = len(nums)
+        k = k % n
+        rotated = [0] * n
+
+        for i in range(n):
+            rotated[(i + k) % n] = nums[i]
+        
+        for i in range(n):
+            nums[i] = rotated[i]
