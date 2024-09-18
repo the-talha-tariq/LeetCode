a1db2b6846bc4e9ff451a4449976d41408c7fe08
Talha Tariq <tariqtalha936@gmail.com>
2024-09-18 21:40:59 +0500

    Best Time to Buy and Sell Stock II

--- main.py
+++ main.py
@@ -104,3 +104,17 @@ class Solution(object):
                 buy = prices[i]
         
         return profit
+
+#Best Time to Buy and Sell Stock II
+class Solution(object):
+    def maxProfit(self, prices):
+       
+        buy = prices[0]
+        profit = 0
+        for i in range(1,len(prices)):
+            if prices[i]>buy:
+                profit  = profit + (prices[i] - buy)
+                buy = prices[i]
+            else:
+                buy = prices[i]
+        return profit
