f27b39c18a8cd50b13a5f7dc81001f1925448278
Talha Tariq <tariqtalha936@gmail.com>
2024-09-23 22:20:07 +0500

    Insert Delete GetRandom O(1)

--- main.py
+++ main.py
@@ -151,4 +151,38 @@ class Solution(object):
             
         
                 
+#Insert Delete GetRandom O(1)
+import random
+class RandomizedSet(object):
+
+    def __init__(self):
+        self.nums = []  
+        self.num_map = {}
+
+    def insert(self, val):
+        if val in self.nums:
+            return False
+        else:
+            self.num_map[val] = len(self.nums)
+            self.nums.append(val)
+            return True
+
+    def remove(self, val):
+        if val in self.nums:
+            last_element = self.nums[-1]
+            index_of_val = self.num_map[val]
+            self.nums[index_of_val] = last_element
+            self.num_map[last_element] = index_of_val
+            self.nums.pop()
+            del self.num_map[val]
+            return True
+    def getRandom(self):
+        return random.choice(self.nums)
         
+
+
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
