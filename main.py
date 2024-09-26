1d1b98097c9789449e844a6263c99241d7818216
Talha Tariq <tariqtalha936@gmail.com>
2024-09-26 23:11:43 +0500

    135. Candy

--- main.py
+++ main.py
@@ -230,3 +230,23 @@ class Solution(object):
         if total_gas < total_cost:
             return -1
         return start_index
+    
+
+#135. Candy
+class Solution(object):
+    def candy(self, ratings):
+        """
+        :type ratings: List[int]
+        :rtype: int
+       """
+        candies = [1] * len(ratings)
+        for i in range(1,len(ratings)):
+            if ratings[i] > ratings[i-1]:
+                candies[i] = candies[i-1] + 1
+
+
+        for i in range(len(ratings)-2, -1, -1):
+            if ratings[i] > ratings[i+1] and candies[i]<=candies[i+1]:
+                candies[i] = candies[i+1]+1
+
+        return sum(candies)
