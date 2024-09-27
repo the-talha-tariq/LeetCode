d4e00f39e64bdbb1e35e52bfa7a1eace96dd8e35
Talha Tariq <tariqtalha936@gmail.com>
2024-09-27 23:34:22 +0500

    Trapping Rain Water

--- main.py
+++ main.py
@@ -250,3 +250,32 @@ class Solution(object):
                 candies[i] = candies[i+1]+1
 
         return sum(candies)
+
+
+#Trapping Rain Water
+class Solution(object):
+    def trap(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        if not height:
+            return 0
+        left_max = [0]*len(height)
+        right_max = [0]*len(height)
+        
+        left_max[0]=height[0]
+        for i in range(1,len(height)-1):
+            left_max[i] = max(left_max[i-1],height[i])
+
+        right_max[len(height)-1]=height[len(height)-1]
+        for i in range(len(height)-2,-1,-1):
+            right_max[i] = max(right_max[i+1],height[i])
+
+        water =0
+        for i in range(len(height)):
+            water += max(0, min(left_max[i], right_max[i]) - height[i])
+        
+
+        
+        return water
