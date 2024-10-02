493e147194561f7fa8cbe21330e7d52f6e925090
Talha Tariq <tariqtalha936@gmail.com>
2024-10-02 22:52:45 +0500

    13. Roman to Integer

--- main.py
+++ main.py
@@ -316,6 +316,35 @@ class Solution(object):
             else:   
                 break
         return word
+
+#13. Roman to Integer
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        roman_map = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+            
+        }
+    
+        total = 0
+        n = len(s)
+        
+        for i in range(n):
+            if i < n - 1 and roman_map[s[i]] < roman_map[s[i + 1]]:
+                total -= roman_map[s[i]]
+            else:
+                total += roman_map[s[i]]
+        
+        return total
                 
 
         
