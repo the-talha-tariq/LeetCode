56951c013411a52432bf65a27e196d4496cea48e
Talha Tariq <tariqtalha936@gmail.com>
2024-10-04 22:52:13 +0500

    6. Zigzag Conversion

--- main.py
+++ main.py
@@ -372,8 +372,31 @@ class Solution(object):
                 return -1
         return -1
 
+#6. Zigzag Conversion
+class Solution(object):
+    def convert(self, s, numRows):
+        if numRows == 1 or numRows >= len(s):
+            return s
 
+        # Create a list to store strings for each row
+        rows = [''] * numRows
+        current_row = 0
+        going_down = False
 
+        for char in s:
+            rows[current_row] += char
+            
+            if current_row == 0 or current_row == numRows - 1:
+                going_down = not going_down
+            
+            if going_down:
+                current_row += 1
+            else:
+                current_row -= 1
+
+        return ''.join(rows)
         
 
         
+
+    
