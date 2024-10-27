e909881d18081d34f66b10ac3d8c8545c23acb6a
Talha Tariq <tariqtalha936@gmail.com>
2024-10-28 00:03:45 +0500

    Update main.py

--- main.py
+++ main.py
@@ -493,3 +493,19 @@ class Solution(object):
             return True
         else:
             return False
+
+#1. Two Sum
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        hashmap = {}  
+        
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in hashmap:
+                return [hashmap[complement], i]
+            hashmap[num] = i
