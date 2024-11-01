04e1d6f9ddfb81b4469a92b93d6c7c5d3e1fba6b
Talha Tariq <tariqtalha936@gmail.com>
2024-11-01 23:17:53 +0500

    167. Two Sum II - Input Array Is Sorted

--- main.py
+++ main.py
@@ -630,3 +630,23 @@ class Solution(object):
             else:
                 return False
         return True
+
+#167. Two Sum II - Input Array Is Sorted
+class Solution(object):
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        i = 0
+        j = len(numbers)-1
+
+        while (i<j):
+            if numbers[i] + numbers[j] == target:
+                return [i+1,j+1]
+            elif numbers[i] + numbers[j] <target:
+                i += 1;
+            else:
+                j -= 1;
+            
