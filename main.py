d9e60031e0e4c7d372296fe66cbf98963d860ac8
Talha Tariq <tariqtalha936@gmail.com>
2024-11-04 23:58:15 +0500

    Skip duplicates for left and right

--- main.py
+++ main.py
@@ -650,3 +650,38 @@ class Solution(object):
             else:
                 j -= 1;
             
+# Skip duplicates for left and right
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        nums.sort()
+        result = []
+        
+        for i in range(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i - 1]:  
+                continue
+                
+            left, right = i + 1, len(nums) - 1
+            
+            while left < right:
+                three_sum = nums[i] + nums[left] + nums[right]
+                
+                if three_sum == 0:
+                    result.append([nums[i], nums[left], nums[right]])
+                    
+                    while left < right and nums[left] == nums[left + 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right - 1]:
+                        right -= 1
+                    
+                    left += 1
+                    right -= 1
+                elif three_sum < 0:
+                    left += 1
+                else:
+                    right -= 1
+        
+        return result
