45f6e32e96887a3379158bbb0d292015cab4f172
Talha Tariq <tariqtalha936@gmail.com>
2024-11-06 22:41:57 +0500

    #20. Valid Parentheses

--- main.py
+++ main.py
@@ -714,3 +714,35 @@ class Solution(object):
                 end -= 1
                 
         return max_area
+
+
+
+#20. Valid Parentheses
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = deque()
+
+        for char in s:
+            if char == "[" or char == "(" or char == "{":
+                stack.append(char)
+            elif char == "]":
+                if not stack or stack[-1] != "[":
+                    return False
+                stack.pop()
+            elif char == ")":
+                if not stack or stack[-1] != "(":
+                    return False
+                stack.pop()
+            elif char == "}":
+                if not stack or stack[-1] != "{":
+                    return False
+                stack.pop()
+
+        if len(stack) == 0:
+            return True
+        else:
+            return False
