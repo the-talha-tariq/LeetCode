4d4251d0e17213c04fb6b4d6e0f7bdc06cc3ad11
Talha Tariq <tariqtalha936@gmail.com>
2024-11-25 23:29:15 +0500

    33. Search in Rotated Sorted Array

--- main.py
+++ main.py
@@ -1005,3 +1005,34 @@ class Solution(object):
                 end = mid - 1
 
         return nums[start]  
+
+
+
+#33. Search in Rotated Sorted Array
+
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        start = 0
+        end = len(nums)-1
+        while start <= end:
+            mid = (start + end)//2
+
+            if nums[mid] == target:
+                return mid
+            
+            if nums[start]<=nums[mid]:
+                if nums[start] <= target < nums[mid]:
+                    end = mid - 1
+                else:
+                    start = mid + 1
+            else:
+                if nums[mid] < target <=nums[end]:
+                    start = mid + 1
+                else:
+                    end = mid - 1
+        return -1
