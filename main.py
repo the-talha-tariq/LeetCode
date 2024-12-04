985b39b0da4500ffda0d7805a6cf85e9e37b0973
Talha Tariq <tariqtalha936@gmail.com>
2024-12-04 23:52:20 +0500

    4. Median of Two Sorted Arrays

--- main.py
+++ main.py
@@ -1186,4 +1186,37 @@ class Solution(object):
         #     else:
         #         updated_string += s[j]
         # return updated_string
+
+
+
+#4. Median of Two Sorted Arrays
+
+class Solution:
+    def findMedianSortedArrays(self, nums1, nums2):
+        if len(nums1) > len(nums2):
+            nums1, nums2 = nums2, nums1
+        
+        m, n = len(nums1), len(nums2)
+        start, end = 0, m
+        
+        while start <= end:
+            partitionX = (start + end) // 2
+            partitionY = (m + n + 1) // 2 - partitionX
             
+            maxLeftX = float('-inf') if partitionX == 0 else nums1[partitionX - 1]
+            minRightX = float('inf') if partitionX == m else nums1[partitionX]
+            
+            maxLeftY = float('-inf') if partitionY == 0 else nums2[partitionY - 1]
+            minRightY = float('inf') if partitionY == n else nums2[partitionY]
+            
+            if maxLeftX <= minRightY and maxLeftY <= minRightX:
+                if (m + n) % 2 == 0:
+                    return (max(maxLeftX, maxLeftY) + min(minRightX, minRightY)) / 2.0
+                else:
+                    return max(maxLeftX, maxLeftY)
+            elif maxLeftX > minRightY:
+                end = partitionX - 1
+            else:
+                start = partitionX + 1
+        
+        raise ValueError("Input arrays are not sorted!")
