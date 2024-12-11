657427af1938480812254682b7b792c4ab3eb54b
Talha Tariq <tariqtalha936@gmail.com>
2024-12-11 23:31:46 +0500

    206. Reverse Linked List

--- main.py
+++ main.py
@@ -1322,3 +1322,26 @@ class Solution(object):
             merged_list = merged_list.next
         
         return dummy.next
+
+
+
+#206. Reverse Linked List
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        current = head
+        prev = None  
+        while current:
+            temp = current.next
+            current.next = prev
+            prev = current
+            current = temp
+        return prev
