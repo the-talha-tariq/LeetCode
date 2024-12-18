4601c15218dda1da7ef1cb6794acd628c8ea4be3
Talha Tariq <tariqtalha936@gmail.com>
2024-12-18 23:27:49 +0500

    2. Add Two Numbers

--- main.py
+++ main.py
@@ -1414,3 +1414,38 @@ class Solution(object):
         return dummy.next
 
 
+#2. Add Two Numbers
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: Optional[ListNode]
+        :type l2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        dummy = ListNode(0)
+        current = dummy
+        carry = 0
+        while l1 or l2:
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
+                
+            total = val1 + val2 + carry
+            carry = total // 10
+            value = total % 10
+
+            current.next = ListNode(value)
+            current = current.next 
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
+        if carry > 0:
+            current.next = ListNode(carry)     
+
+        return dummy.next
