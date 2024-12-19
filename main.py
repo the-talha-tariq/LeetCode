d8eba3d6d61ccce9b3e6708ab20102971e2fee0e
Talha Tariq <tariqtalha936@gmail.com>
2024-12-19 23:07:27 +0500

    141. Linked List Cycle

--- main.py
+++ main.py
@@ -1449,3 +1449,30 @@ class Solution(object):
             current.next = ListNode(carry)     
 
         return dummy.next
+
+
+#141. Linked List Cycle
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        
+        slow = head
+        fast = head
+
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+
+            if slow == fast:
+                return True
+        return False
