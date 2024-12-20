dcceecd67b202081e2472a923a4867643edc1e0a
Talha Tariq <tariqtalha936@gmail.com>
2024-12-20 23:25:50 +0500

    287. Find the Duplicate Number

--- main.py
+++ main.py
@@ -1476,3 +1476,27 @@ class Solution(object):
             if slow == fast:
                 return True
         return False
+
+#287. Find the Duplicate Number
+class Solution(object):
+    def findDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        slow = nums[0]
+        fast = nums[0]
+
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+
+            if slow == fast:
+                break
+        slow = nums[0]
+        while slow!=fast:
+            slow = nums[slow]
+            fast = nums[fast]
+
+        return slow
+           
