d9b08d391375fb96188a4edd0648165c806cf848
Talha Tariq <tariqtalha936@gmail.com>
2024-12-30 23:47:25 +0500

    543. Diameter of Binary Tree

--- main.py
+++ main.py
@@ -1583,3 +1583,35 @@ class Solution(object):
         lHeight = self.maxDepth(root.left)
         rHeight = self.maxDepth(root.right)
         return max(lHeight, rHeight) + 1
+
+
+#543. Diameter of Binary Tree
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def diameterOfBinaryTree(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: int
+        """
+        self.diameter = 0  
+
+        def height(node):
+            if not node:
+                return 0  
+
+            left_height = height(node.left)
+            right_height = height(node.right)
+
+            self.diameter = max(self.diameter, left_height + right_height)
+
+            return max(left_height, right_height) + 1
+
+        height(root)  
+        return self.diameter
+        
