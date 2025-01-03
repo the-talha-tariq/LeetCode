7e018fb200bd2375fc43236ee33aeaf365c9d647
Talha Tariq <tariqtalha936@gmail.com>
2025-01-03 23:23:06 +0500

    572. Subtree of Another Tree

--- main.py
+++ main.py
@@ -1670,3 +1670,34 @@ class Solution(object):
         return self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right)
 
             
+#572. Subtree of Another Tree
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isSubtree(self, root, subRoot):
+        """
+        :type root: Optional[TreeNode]
+        :type subRoot: Optional[TreeNode]
+        :rtype: bool
+        """
+        if not root:
+            return False
+        if self.isSameTree(root, subRoot): 
+            return True
+
+        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
+    def isSameTree(self, p, q):
+        if not p and not q:
+            return True
+        if not p or not q:
+            return False
+        if p.val != q.val:
+            return False
+        
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        
