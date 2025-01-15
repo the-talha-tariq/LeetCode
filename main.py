fbb5cc63f628a52907f85d2acd60bb62c0de1e0a
Talha Tariq <tariqtalha936@gmail.com>
2025-01-16 00:07:32 +0500

    35. Search Insert Position

--- main.py
+++ main.py
@@ -1783,3 +1783,27 @@ class Solution(object):
             C.append(common_count)
 
         return C
+
+#35. Search Insert Position
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        if target < nums[0]:
+            return 0
+        
+        if len(nums) == 1 and target <= nums[0]:
+            return 0
+        elif len(nums) == 1 and target > nums[0]:
+            return 1
+
+        for i in range(len(nums)-1):
+            if nums[i] == target:
+                return i
+            elif nums[i] < target <= nums[i+1]:
+                return i+1
+            
+        return len(nums)
