5998c5a9574ab6ac572ab1ff44d048cd682769f3
Talha Tariq <tariqtalha936@gmail.com>
2025-04-08 00:41:18 +0500

    416. Partition Equal Subset Sum

--- main.py
+++ main.py
@@ -1886,3 +1886,28 @@ class Solution(object):
             return -1
 
         return operations
+
+
+#416. Partition Equal Subset Sum
+
+class Solution(object):
+    def canPartition(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        total = sum(nums)
+
+        if total % 2 != 0:
+            return False
+
+        target = total // 2
+        n = len(nums)
+        dp = [False] * (target + 1)
+        dp[0] = True  # We can always form sum 0
+
+        for num in nums:
+            for i in range(target, num - 1, -1):
+                dp[i] = dp[i] or dp[i - num]
+
+        return dp[target]
