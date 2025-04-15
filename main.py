ae870b8d1a026f3fd8be634121b70475c546b870
Talha Tariq <tariqtalha936@gmail.com>
2025-04-15 23:59:09 +0500

    53. Maximum Subarray

--- main.py
+++ main.py
@@ -1926,3 +1926,19 @@ class Solution(object):
 
         greater_than_k = set(num for num in nums if num > k)
         return len(greater_than_k)
+
+
+#53. Maximum Subarray
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        current_sum = nums[0]  
+        max_sum = nums[0] 
+        for i in range(1, len(nums)):
+            current_sum = max(nums[i], current_sum + nums[i])
+            max_sum = max(max_sum, current_sum)  
+
+        return max_sum
