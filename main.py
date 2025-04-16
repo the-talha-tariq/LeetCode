95a1b98d006172a9653f6b5844922ed31885ef66
Talha Tariq <tariqtalha936@gmail.com>
2025-04-16 23:55:05 +0500

    383. Ransom Note

--- main.py
+++ main.py
@@ -1942,3 +1942,21 @@ class Solution(object):
             max_sum = max(max_sum, current_sum)  
 
         return max_sum
+
+
+#383. Ransom Note
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        """
+        :type ransomNote: str
+        :type magazine: str
+        :rtype: bool
+        """
+        ransom_count = Counter(ransomNote)
+        magazine_count = Counter(magazine)
+    
+        for char in ransom_count:
+            if ransom_count[char] > magazine_count.get(char, 0):
+                return False
+        
+        return True
